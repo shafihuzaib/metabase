1180453ae529ac7d3c45296b5e767f4eeff4b5f2
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-07-23 14:20:27 -0700

    lint

--- frontend/src/metabase/containers/Overworld.jsx
+++ frontend/src/metabase/containers/Overworld.jsx
@@ -117,7 +117,9 @@ class Overworld extends React.Component {
                         key={`${pin.model}-${pin.id}`}
                       >
                         <Link
-                          data-metabase-event={`Homepage;Pinned Item Click;Pin Type ${pin.model}`}
+                          data-metabase-event={`Homepage;Pinned Item Click;Pin Type ${
+                            pin.model
+                          }`}
                           to={Urls.dashboard(pin.id)}
                           hover={{ color: normal.blue }}
                         >
@@ -196,7 +198,9 @@ class Overworld extends React.Component {
                         <Link
                           to={`browse/${database.id}`}
                           hover={{ color: normal.blue }}
-                          data-metabase-event={`Homepage;Browse DB Clicked; DB Type ${database.engine}`}
+                          data-metabase-event={`Homepage;Browse DB Clicked; DB Type ${
+                            database.engine
+                          }`}
                         >
                           <Box p={3} bg={colors["bg-medium"]}>
                             <Icon
